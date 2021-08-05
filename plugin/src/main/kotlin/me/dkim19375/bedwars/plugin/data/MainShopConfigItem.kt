9e8dd6a1f6c4adaaa9b3606c235446ebd9929c7d
dkim19375 <miner8mk@gmail.com>
2021-08-04 23:20:25 -0400

    Update license

--- plugin/src/main/kotlin/me/dkim19375/bedwars/plugin/data/MainShopConfigItem.kt
+++ plugin/src/main/kotlin/me/dkim19375/bedwars/plugin/data/MainShopConfigItem.kt
@@ -1,3 +1,21 @@
+/*
+ *     Bedwars, a minigame for spigot
+ *     Copyright (C) 2021  dkim19375
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.dkim19375.bedwars.plugin.data
 
 import me.dkim19375.bedwars.plugin.gui.MainShopGUI
